2a61aac4b4aa5df6eec154b212c0ff658a578e96
joemface <joemface@yahoo.com>
2020-05-23 13:14:34 -0400

    missing parenthesis

--- books.py
+++ books.py
@@ -2,4 +2,4 @@ import requests
 
 res = requests.get("https://www.goodreads.com/book/review_counts.json",
                    params={"key": "ogA93yMNFmZJdJH9igJxxQ", "isbns": "9781632168146"})
-print(res.json()
+print(res.json())
